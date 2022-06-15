9d360d14cce34448f41fd0a56025d191327ab8c0
jakobln <62882745+jakobln@users.noreply.github.com>
2022-06-15 13:43:00 +0200

    Add project files.

---
+++ RESTService/Controllers/WindsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RESTService.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RESTService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WindsController : ControllerBase
+    {
+        //Internal reference to a manager
+        private WindsManager _manager = new WindsManager();
+
+        //Simply returns all wind data
+        //Return a 204 if no sensors has send any data
+        //Instead of 204 we could use 404, but that is seen as an error
+        // GET: api/<WindsController>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [HttpGet]
+        public ActionResult<IEnumerable<Wind>> Get()
+        {
+            IEnumerable<Wind> result = _manager.GetAll();
+            if (result.Count() > 0)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+
+        //Adds a wind object, delegates the responsibility of added id to the manager
+        //Here we use the 201 status code to tell the user that it has been created, but because we have no access to the specific sensordata, we just return the Uri to the get method
+        // POST api/<WindsController>
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [HttpPost]
+        public ActionResult Post([FromBody] Wind value)
+        {
+            _manager.Add(value);
+            return Created("/api/speed", value);
+        }
+    }
+}
---
+++ RESTService/Managers/WindsManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTService.Managers
+{
+    public class WindsManager
+    {
+        //A simple int to keep track of ID's
+        private static int _nextID = 1;
+
+        private static List<Wind> _data = new List<Wind>()
+        {
+            new Wind { Id = _nextID++, Direction = "North", Speed = 1},
+            new Wind { Id = _nextID++, Direction = "East", Speed = 0},
+            new Wind { Id = _nextID++, Direction = "South", Speed = 1},
+            new Wind { Id = _nextID++, Direction = "West", Speed = 2},
+            new Wind { Id = _nextID++, Direction = "North", Speed = 0}
+        };
+
+        //Return a copy of the list
+        public List<Wind> GetAll()
+        {
+            return new List<Wind>(_data);
+        }
+
+        //Adds an object to the list, and assign a unique ID
+        public int Add(Wind newData)
+        {
+            newData.Id = _nextID++;
+            _data.Add(newData);
+            return newData.Id;
+        }
+
+    }
+}
